f74ef607d8f644f59e0a5ace088058acf2aeb05d
himanshu kumawat <himanshu9772kumawat@Gmail.com>
2024-11-29 18:38:58 +0530

    Delete src directory

--- src/Pages/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import adminReducer from './reducers/adminSlice';
-
-
-export const store = configureStore({
-  reducer: {
-    admin : adminReducer,
-  },
-})
